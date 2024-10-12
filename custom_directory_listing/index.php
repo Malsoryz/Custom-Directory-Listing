f8366a6ac2469454e8d5c1686121762a677efdb9
Malsoryzzz <ikmalalansory@gmail.com>
2024-10-12 20:59:24 +0800

    lightweight update

--- custom_directory_listing/index.php
+++ custom_directory_listing/index.php
@@ -5,29 +5,30 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Dokumen</title>
+  <title>Directory listing</title>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   <link rel="stylesheet" href="RootRes/css/style.css" type="text/css" media="all" />
 </head>
-<body class="theme-light">
+<body class="enable-theme">
   <nav class="navbar etr-shadow cusborder-no-hover">
     <h1>ETERNITY</h1>
-    <button class="theme-btn" id="theme-btn"><i class="fa-solid fa-circle-half-stroke"></i></i>
+    <input type="checkbox" id="theme-checkbox"/>
+    <label for="theme-checkbox" id="theme-label">
+      <i class="fa-solid fa-circle-half-stroke"></i>
+    </label>
   </nav>
   <section class="indexlist">
-    <?php
-      foreach ($items as $item) {
-        $icon = ( $item[1] ) ? "fa-folder" : "fa-file";
-        echo <<< EOD
-          <a href="$item[0]" class="item">
-            <div class="item-container etr-shadow etr-corner cusborder">
-              <h3>$item[0]<i class="flrght fa-regular $icon"></i></h3>
-            </div>
-          </a>
-        EOD;
-      };
-    ?>
+    <?php foreach ($items as $item):?>
+      <?php $icon = ( $item[1] ) ? "fa-folder" : "fa-file"?>
+        <a href="<?=$item[0]?>" class="item">
+          <div class="item-container etr-shadow etr-corner cusborder">
+            <h3>
+              <span class="files-label"><?=$item[0]?></span>
+              <i class="flrght fa-regular <?=$icon?>"></i>
+            </h3>
+          </div>
+        </a>
+    <?php endforeach?>
   </section>
-  <script src="RootRes/js/script.js" type="text/javascript" charset="utf-8"></script>
 </body>
 </html>
